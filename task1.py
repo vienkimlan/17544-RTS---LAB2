724e6dcacd5b54c7cb6997d7fed5d15a14badd35
haoidk20002 <91824715+haoidk20002@users.noreply.github.com>
2023-05-30 11:18:16 +0700

    Update task1.py

--- task1.py
+++ task1.py
@@ -22,39 +22,38 @@ class Task1:
 
     def Task_Run(self):
         # Disable scientific notation for clarity
-        while True:
-            # Grab the webcamera's image.
-            ret, image = self.camera.read()
+        # Grab the webcamera's image.
+        ret, image = self.camera.read()
 
-            # Resize the raw image into (224-height,224-width) pixels
-            image = cv2.resize(image, (224, 224), interpolation=cv2.INTER_AREA)
+        # Resize the raw image into (224-height,224-width) pixels
+        image = cv2.resize(image, (224, 224), interpolation=cv2.INTER_AREA)
 
-            # Show the image in a window
-            cv2.imshow("Webcam Image", image)
+        # Show the image in a window
+        cv2.imshow("Webcam Image", image)
 
-            # Make the image a numpy array and reshape it to the models input shape.
-            image = np.asarray(image, dtype=np.float32).reshape(1, 224, 224, 3)
+        # Make the image a numpy array and reshape it to the models input shape.
+        image = np.asarray(image, dtype=np.float32).reshape(1, 224, 224, 3)
 
-            # Normalize the image array
-            image = (image / 127.5) - 1
+        # Normalize the image array
+        image = (image / 127.5) - 1
 
-            # Predicts the model
-            prediction = self.model.predict(image)
-            index = np.argmax(prediction)
-            class_name = self.class_names[index]
-            confidence_score = prediction[0][index]
+        # Predicts the model
+        prediction = self.model.predict(image)
+        index = np.argmax(prediction)
+        class_name = self.class_names[index]
+        confidence_score = prediction[0][index]
 
-            # Print prediction and confidence score
-            print("Class:", class_name[2:], end="")
-            print("Confidence Score:", str(np.round(confidence_score * 100))[:-2], "%")
+        # Print prediction and confidence score
+        print("Class:", class_name[2:], end="")
+        print("Confidence Score:", str(np.round(confidence_score * 100))[:-2], "%")
 
-            # Listen to the keyboard for presses.
-            keyboard_input = cv2.waitKey(1)
+        # Listen to the keyboard for presses.
+        keyboard_input = cv2.waitKey(1)
 
-            # 27 is the ASCII for the esc key on your keyboard.
-            if keyboard_input == 27:
-                self.camera.release()
-                cv2.destroyAllWindows()
-            return
+        # 27 is the ASCII for the esc key on your keyboard.
+        if keyboard_input == 27:
+            self.camera.release()
+            cv2.destroyAllWindows()
+        return
 
         
